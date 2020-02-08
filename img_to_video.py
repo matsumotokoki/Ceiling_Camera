a596ad13b28636c29e9081a7447996bfc84e2064
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2020-02-09 01:18:14 +0900

    fix

--- img_to_video.py
+++ img_to_video.py
@@ -4,9 +4,15 @@ import datetime
 fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
 video = cv2.VideoWriter('video'+ datetime.datetime.today().strftime("%Y%M%d%H%M%S") +'.mp4', fourcc, 20.0, (640, 480))
 
+img = cv2.imread('image1.jpg')
+# hight, width, depth = img.shape[:3]
+
 for i in range(1, 700):
     # img = cv2.imread('image{0:03d}.png'.format(i+1))
-    img = cv2.imread('image'+ str(i+1) +'.png')
+    img = cv2.imread('./jpg_file/datas'+ str(i+1) +'.jpg')
+    if img is None:
+        print("can't read")
+        break
     img = cv2.resize(img, (640,480))
     video.write(img)
 
