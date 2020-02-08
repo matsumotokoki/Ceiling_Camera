5d85e5946159c1458dcdfb36b73391c558fc6e2f
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2020-02-09 01:15:06 +0900

    add make video func

--- contant_camera.py
+++ contant_camera.py
@@ -21,14 +21,29 @@ dist[0,5] = -41.7271568
 dist[0,6] = 503.1987806
 dist[0,7] = 294.3816648
 
+seconds = 100
+
 camera = cv2.VideoCapture(0)
 date = datetime.datetime.today().strftime("%Y%M%d%H%M")
 dir_name = "jpg_file"+str(date)
 os.mkdir(dir_name)
-for i in range(700):
+for i in range(seconds):
     print(str(i+1) + " times")
     return_value, image = camera.read()
     image = cv2.undistort(image,mtx,dist)
     cv2.imwrite('./' + dir_name +'/datas'+str(i)+'.jpg', image)
     time.sleep(1) 
 del(camera)
+
+fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
+video = cv2.VideoWriter('./' + dir_name + '/video_'+ date +'.mp4', fourcc, 20.0, (640, 480))
+
+for i in range(seconds):
+    img = cv2.imread('./' + dir_name + '/datas'+ str(i+1) +'.jpg')
+    if img is None:
+        print("can't read")
+        break
+    img = cv2.resize(img, (640,480))
+    video.write(img)
+
+video.release()
